eac028de133716edb6f82adda975381dc50e87fa
Heath Keene <hkeene@gmail.com>
2018-10-11 21:02:31 -0500

    Added playground with serial logger

---
+++ playground/input_worker.py
@@ -0,0 +1,26 @@
+FRAME_DLE = b'\x10'
+FRAME_STX = b'\x02'
+FRAME_ETX = b'\x03'
+
+FRAME_START = FRAME_DLE + FRAME_STX
+FRAME_END   = FRAME_DLE + FRAME_ETX
+
+frame = bytearray()
+
+with open("idle.bin", "rb") as f:
+
+    while True: #Will break out of this once the file ends
+        #First prime the bytearray with two reads
+        frame += f.read(2)
+        #Iterate over frame until it is the start
+        while frame != FRAME_START:
+            print("missed start frame")
+            frame.pop(0)
+            frame += f.read(1)
+
+        while not frame.endswith(FRAME_END):
+            frame += f.read(1)
+
+        #Push frame to queue for processing
+        print(frame)
+        frame.clear()
---
+++ playground/serial_logger.py
@@ -0,0 +1,25 @@
+import serial, time, io, datetime
+from serial import Serial
+
+ser = serial.Serial(
+    port = '/dev/ttyUSB0',\
+    baudrate = 19200,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_TWO,\
+    bytesize=serial.EIGHTBITS,\
+    timeout=0)
+
+
+print("Connected to: " + ser.portstr)
+
+seq = []
+
+for x in range (100):
+    seq.append(ser.read(1))
+
+ser.close()
+
+with open("idle.bin", "rb") as f:
+    f.write(seq)
+
+f.close()
