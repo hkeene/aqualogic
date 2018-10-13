13346cf46b06753ec2930bf061745fd7c0c186d1
Heath Keene <hkeene@gmail.com>
2018-10-13 15:53:31 -0500

    Added another serial logger. This one will test ending gracefully.

--- playground/serial_logger.py
+++ playground/serial_logger.py
@@ -1,29 +1,58 @@
 import serial
 
-#ser = serial.Serial(
-#    port = '/dev/ttyUSB0',\
-#    baudrate = 19200,\
-#    parity=serial.PARITY_NONE,\
-#    stopbits=serial.STOPBITS_TWO,\
-#    bytesize=serial.EIGHTBITS)
+FRAME_DLE = b'\x10'
+FRAME_STX = b'\x02'
+FRAME_ETX = b'\x03'
 
-#ser = serial.Serial('/dev/ttyUSB0')
+FRAME_START = FRAME_DLE + FRAME_STX
+FRAME_END   = FRAME_DLE + FRAME_ETX
 
-ser = serial.Serial(
-    port = '/dev/ttyUSB0',\
-    baudrate = 19200)
-#    parity=serial.PARITY_NONE,\
-#    stopbits=serial.STOPBITS_TWO,\
-#    bytesize=serial.EIGHTBITS)
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-print("Connected to: " + ser.portstr)
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
 
-f = open("log.bin", "wb")
+frame = bytearray()
 
-for x in range(4000):
-    y = ser.read(1)
-    #print(y)
-    f.write(y)
+if __name__ == '__main__':
+    killer = GracefulKiller()
+    ser = serial.Serial(
+        port = '/dev/ttyUSB0',\
+        baudrate = 19200)
 
-ser.close()
-f.close()
+    print("Connected to: " + ser.portstr)
+
+    f = open("log.bin", "wb")
+
+    print("Opened log file")
+    print("Type CTNL-Z to stop")
+
+    while True:
+        #First prime the bytearray with two reads
+        frame += ser.read(2)
+        #Iterate over frame until it is the start
+        while frame != FRAME_START:
+            print("missed start frame")
+            frame.pop(0)
+            frame += ser.read(1)
+
+        while not frame.endswith(FRAME_END):
+            frame += ser.read(1)
+
+        #Push frame to queue for processing
+        f.write(frame)
+        frame.clear()
+
+
+
+        if killer.kill_now:
+          break
+
+    print ("End of the program. I was killed gracefully")
+
+    ser.close()
+    f.close()
---
+++ playground/trap_key.py
@@ -0,0 +1,20 @@
+import signal
+import time
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
+if __name__ == '__main__':
+  killer = GracefulKiller()
+  print("I'm going into the loop")
+  while True:
+    if killer.kill_now:
+      break
+
+  print ("End of the program. I was killed gracefully")
