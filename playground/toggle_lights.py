87d4684203775cbd29a823858698fc8d4607d5c8
Heath Keene <hkeene@gmail.com>
2018-10-14 10:51:14 -0500

    Another attempt to turn on the lights in a simple example.

--- playground/toggle_lights.py
+++ playground/toggle_lights.py
@@ -8,6 +8,9 @@ FRAME_ETX = b'\x03'
 FRAME_START = FRAME_DLE + FRAME_STX
 FRAME_END   = FRAME_DLE + FRAME_ETX
 
+FRAME_TYPE_KEY_EVENT = b'\x00\x03'
+LIGHTS = b'\x00\x01'
+
 FRAME_LIGHTS = b'\x10\x02\x01\x02\x68\x00\xfe\x01\x00\x00\x00\x00\x01\x7c\x10\x03'
 FRAME_TYPE_KEEP_ALIVE = b'\x10\x02\x01\x01\x00\x14\x10\x03'
 
@@ -21,9 +24,37 @@ class GracefulKiller:
     def exit_gracefully(self,signum, frame):
         self.kill_now = True
 
+def _append_data(self, frame, data):
+    for byte in data:
+        frame += byte
+        if byte == self.FRAME_DLE:
+            frame += 0
+
 frame = bytearray()
 
 if __name__ == '__main__':
+    # Create frame to send
+    lights = bytearray()
+    lights += FRAME_DLE
+    lights += FRAME_STX
+
+    lights += FRAME_TYPE_KEY_EVENT
+
+    lights += LIGHTS
+    lights += LIGHTS
+
+    crc = 0
+    for byte in lights:
+        crc += byte
+
+    lights += crc.to_bytes(2, byteorder='big')
+
+    lights += FRAME_DLE
+    lights += FRAME_ETX
+
+    print(lights)
+
+
     do_once = True
     killer = GracefulKiller()
     ser = serial.Serial(
@@ -52,7 +83,7 @@ if __name__ == '__main__':
         #Push frame to queue for processing
         if (frame == FRAME_TYPE_KEEP_ALIVE) and do_once :
             print("sending light command")
-            ser.write(FRAME_LIGHTS)
+            ser.write(lights)
             do_once = False
         f.write(frame)
         frame.clear()
